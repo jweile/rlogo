b5b4d2418fa79169ab1396a261aec065e94c4907
Jochen Weile <jochenweile@gmail.com>
2018-10-25 12:59:01 -0400

    drawSimpleLogo function

--- R/rlogo.R
+++ R/rlogo.R
@@ -25,3 +25,47 @@ drawLetter <- function(letter,x0,y0,x1,y1,col=par("fg"),bgcol="white") {
 		}))
 	}
 }
+
+#' single letter amino acid codes
+#' @export
+aaLetters <- c("A","V","L","I","M","F","Y","W","H","K","R","D","E","N","Q","S","T","G","C","P")
+
+#' single letter nucleotide codes
+#' @export
+ncLetters <- c("A","C","G","T")
+
+#' A color scheme for nucleotides
+#' @export
+ncColors <- setNames(
+	c("steelblue3","darkolivegreen3","gold","firebrick3"),
+	ncLetters
+)
+
+#' A color scheme for amino acids
+#' @export
+aaColors <- c(
+	A="steelblue1",V="steelblue2",L="steelblue3",I="steelblue",M="steelblue4",
+	F="turquoise1",Y="turquoise2",W="turquoise3",R="firebrick1",H="firebrick2",K="firebrick3",
+	D="violetred1",E="violetred2",S="darkolivegreen1",T="darkolivegreen2",
+	N="darkolivegreen3",Q="darkolivegreen4",G="darkgoldenrod1",C="darkorange",P="gold"
+)
+
+#' draw a simple weblogo
+#' 
+#' draws a simple web logo for a matrix of values.
+#' 
+#' @param data a numerical matrix with rownames corresponding to the letters to be drawn.
+#' @param col a named vector of colors to be used for each letter.
+#' @param lttrs the letters corresponding to the rows in the matrix, defaults to the matrix's colnames.
+#' @export
+#'
+drawSimpleLogo <- function(data,col,lttrs=rownames(data)) {
+	plot(0,type="n",xlim=c(0,ncol(data)+1),ylim=c(0,max(colSums(data,na.rm=TRUE),na.rm=TRUE)))
+	for (i in 1:ncol(data)) {
+		top <- sum(data[,i])
+		for (j in 1:nrow(data)) {
+			drawLetter(lttrs[[j]],i-.5,top-data[j,i],i+.5,top,col=col[[lttrs[[j]]]])
+			top <- top-data[j,i]
+		}
+	}
+}
